1a265fe1d3ac7b183406218207c8f648953af1b6
Terje Norderhaug <terje@in-progress.com>
2019-11-26 17:12:43 -0800

    Enable filter/search

--- src/Main.js
+++ src/Main.js
@@ -1,60 +1,114 @@
 import React, {useRef} from 'react'
-import { useFile } from 'react-blockstack'
+import { useFile, useFilesList } from 'react-blockstack'
+import {useTable,
+        useGroupBy,
+        useFilters,
+        useSortBy,
+        useExpanded} from 'react-table'
 
-const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
 
-function Profile ({ person }) {
   return (
-   <div className="Profile">
-    <div className="avatar-section">
-      <img src={ (person && person.avatarUrl()) || avatarFallbackImage }
-           className="img-rounded avatar" id="avatar-image" alt="Avatar"/>
-    </div>
-    <h1>Hello, <span id="heading-name">{ (person && person.name()) || 'Nameless Person' }</span>!</h1>
-   </div>
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
   )
 }
 
-function NoteField ({placeholder}) {
-  const [note, setNote] = useFile("note")
-  const textfield = useRef()
-  const spinner = useRef()
-  const saveAction = () => {
-    spinner.current.classList.remove('d-none')
-    setNote(textfield.current.value)
-    setTimeout(() => spinner.current.classList.add('d-none'), 1500)
-  }
-  return(
-    <div className="NoteField input-group ">
-      <div className="input-group-prepend">
-        <span className="input-group-text">Note</span>
-      </div>
-      <input type="text" ref={textfield} className="form-control" disabled={note === undefined}
-             defaultValue={ note || ""} placeholder={placeholder}
-             onKeyUp={(e) => {if (e.key === "Enter") saveAction()}}/>
-      <div className="input-group-append">
-        <button className="btn btn-outline-secondary" type="button"
-                disabled={!setNote} onClick={saveAction}>
-          <div ref={spinner} role="status"
-               className="d-none spinner-border spinner-border-sm text-info align-text-top mr-2"/>
-          Save
-        </button>
-      </div>
-    </div>
+function Table({ columns, data, filterTypes }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state
+  } = useTable({
+    columns,
+    data,
+    filterTypes
+  }, useFilters)
+  return (
+  <>
+    <code>{JSON.stringify(state.filters, null, 2)}</code>
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>
+                {column.render('Header')}
+                <div>{column.canFilter ? column.render('Filter') : null}</div>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(
+          (row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )}
+        )}
+      </tbody>
+    </table>
+  </>
   )
 }
+
+const initColumns = [
+    {
+      Header: 'Files',
+      Filter: DefaultColumnFilter,
+      columns: [
+        {
+          Header: 'Name',
+          accessor: 'fileName',
+          Filter: DefaultColumnFilter,
+        },
+        {
+          Header: 'Size',
+          accessor: 'fileSize',
+          Filter: DefaultColumnFilter,
+        },
+      ],
+    }]
+
+const initFilters = {
+      text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true
+        })
+      },
+    }
 
 export default function Main ({ person }) {
+  const [files] = useFilesList()
+  const columns = React.useMemo(() => initColumns)
+  const data = files.map((name) => ({fileName: name, fileSize: 0}))
+  const filterTypes = React.useMemo(() => initFilters)
   return (
-    <main className="panel-welcome">
-      <div className="text-center">
-        <Profile person={person}/>
-      </div>
-      <div className="lead row mt-5">
-        <div className="mx-auto col-md-8 col-lg-6 px-3">
-          <NoteField placeholder="to your future self..."/>
-        </div>
-      </div>
+    <main>
+      <Table columns={columns} data={data} filterTypes={filterTypes}/>
     </main>
   )
 }
