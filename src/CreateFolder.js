9a3c0f94efec76921ca23cb440d9a9e5e6d879ed
Terje Norderhaug <terje@in-progress.com>
2020-01-08 12:54:28 -0800

    Resolve blocked form submission due to sandboxing

--- src/CreateFolder.js
+++ src/CreateFolder.js
@@ -14,26 +14,30 @@ function NewFolderModal (props) {
     submit
   } = props
   const inputRef = useRef()
-  const createAction = () => {
+  const createAction = (e) => {
     const value = inputRef.current.value
     console.log("SUBMIT:", value)
+    e.stopPropagation()
+    e.preventDefault()
     submit && submit(value)
     toggle()
     return false
   }
   return (
     <Modal isOpen={open} toggle={toggle} className={className}>
-      <Form onSubmit={e => {e.stopPropagation();e.preventDefault();createAction()}}>
+      <Form action="#" onSubmit={createAction}>
         <ModalHeader toggle={toggle}>New Folder</ModalHeader>
         <ModalBody>
           <FormGroup>
             {false && <Label for="exampleEmail">Email</Label>}
             <Input innerRef={inputRef} type="text" name="folder" placeholder="Name of folder"/>
           </FormGroup>
+          <div className="mr-auto text-muted"><small>Empty folders
+          are deleted&nbsp;on&nbsp;reload.</small></div>
         </ModalBody>
         <ModalFooter>
           <Button color="secondary" onClick={toggle}>Cancel</Button>
-          <Button color="primary" type="submit">Create</Button>{' '}
+          <Button color="primary" type="submit" onClick={createAction}>Create</Button>{' '}
         </ModalFooter>
       </Form>
     </Modal>
